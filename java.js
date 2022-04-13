423fb150cee4c600090e420c951f368318e2fd91
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-12 22:13:15 -0400

    First Commit...have done alot. Created basic java functionality for calculating operations. Made html skeleton and created the calculator and also added the CSS for it. I will commit much more often going forward.

---
+++ java.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    console.log(num1 - num2);
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    console.log(num1 * num2);
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    console.log(num1 / num2);
+    return num1 / num2;
+}
+
+function operate(num1, num2, operation) {
+    operation(num1, num2);
+}
