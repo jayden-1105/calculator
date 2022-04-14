f696c8d141d9b5d32257f6cacbb5f7016c6f5010
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-14 17:30:48 -0400

    Added values to the operators.

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+
 const btnAdd = document.querySelector('.addition');
 const btnSubtract = document.querySelector('.subtract');
 const btnDivide = document.querySelector('.divide');
@@ -15,25 +16,79 @@ const btnNine = document.querySelector('.nine');
 const btnDecimal = document.querySelector('.decimal');
 const btnZero = document.querySelector('.zero');
 const btnClr = document.querySelector('.clear');
-const answer = document.querySelector('.answer');
+const answerText = document.querySelector('.answer');
 
-btnAdd.onclick = () => console.log('Add');
-btnSubtract.onclick = () => console.log('Subtract');
-btnDivide.onclick = () => console.log('Divide');
-btnMultiply.onclick = () => console.log('Multiply');
-btnEqual.onclick = () => console.log('Equal');
-btnOne.onclick = () => console.log('1');
-btnTwo.onclick = () => console.log('2');
-btnThree.onclick = () => console.log('3');
-btnFour.onclick = () => console.log('4');
-btnFive.onclick = () => console.log('5');
-btnSix.onclick = () => console.log('6');
-btnSeven.onclick = () => console.log('7');
-btnEight.onclick = () => console.log('8');
-btnNine.onclick = () => console.log('9');
-btnDecimal.onclick = () => console.log('.');
-btnZero.onclick = () => console.log('0');
-btnClr.onclick = () => console.log('clr');
+btnAdd.onclick = (e) => createNumber(e);
+btnSubtract.onclick = (e) => createNumber(e);
+btnDivide.onclick = (e) => createNumber(e);
+btnMultiply.onclick = (e) => createNumber(e);
+btnEqual.onclick = (e) => createNumberString(e);
+btnOne.onclick = (e) => createNumberString(e);
+btnTwo.onclick = (e) => createNumberString(e);
+btnThree.onclick = (e) => createNumberString(e);
+btnFour.onclick = (e) => createNumberString(e);
+btnFive.onclick = (e) => createNumberString(e);
+btnSix.onclick = (e) => createNumberString(e);
+btnSeven.onclick = (e) => createNumberString(e);
+btnEight.onclick = (e) => createNumberString(e);
+btnNine.onclick = (e) => createNumberString(e);
+btnDecimal.onclick = (e) => createNumberString(e);
+btnZero.onclick = (e) => createNumberString(e);
+btnClr.onclick = (e) => console.log('clr');
+
+
+let currentNumber = "";
+let firstNumber = "";
+let secondNumber = "";
+let answer = "";
+let currentOperation = "";
+
+
+// Takes input from buttons pressed and concats a string together to make a numbe.
+function createNumberString(e) {
+    currentNumber = currentNumber.concat(e.target.value);
+    console.log(currentNumber);
+
+}
+
+// Takes the string created in createNumberString and converts it to a number.
+// Assigns the new number to either first or second number depending on if firstNumber is blank or not.
+// Resets currentNumber to blank so createNumberScreen can continue collecting a second number.
+function createNumber(e) {
+    if(firstNumber === "") {
+        firstNumber = parseInt(currentNumber);
+        console.log(`first number ${firstNumber}`);
+    }
+    else {
+        secondNumber = parseInt(currentNumber);
+        console.log(`second number ${secondNumber}`);   
+    }
+    currentNumber = "";
+}
+
+// function getOperation(e) {
+//     let operator = e.target.value
+//     console.log(operator);
+//     if(operator="+") {
+//         return add();
+//     }
+//     else if(operator="-") {
+//         return subtract();
+//     }
+//     else if(operator="*") {
+//         return multiply();
+//     }
+//     else { 
+//         return divide();
+//     }
+// }
+function operate(num1, num2, operation) {
+    return operation(num1, num2);
+}
+
+
+
+// Operator Functions
 
 function add(num1, num2) {
     console.log(num1 + num2);
@@ -55,10 +110,8 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, num2, operation) {
-    operation(num1, num2);
-}
+
 
 function updateAnswer(answer) {
-    answer.innerHTML = answer;
+    answerText.innerHTML = answer;
 }
